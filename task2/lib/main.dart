b4480edfca81d0bce53058c612f9671b0a45bc94
bawanisandunika <bawanisandunika51@gmail.com>
2024-10-18 21:16:35 +0530

    updates

--- task2/lib/main.dart
+++ task2/lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+// ignore: unused_import
 import './screens/welcome_screen.dart'; // Import the WelcomeScreen
 import './screens/login_screen.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
